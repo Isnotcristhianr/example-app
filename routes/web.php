d9ee9351965b693717faac82de99b567d4f1b612
Cristhian David Recalde <89264838+Isnotcristhianr@users.noreply.github.com>
2023-11-22 09:02:56 -0500

    Add routes for courses

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Ruta de tipo get
+Route::get('cursos', function () {
+    return 'Bienvenido a la página de cursos';
+});
+
+//enrutamiento a una vista
+Route::get('cursos/create', function () {
+    return 'En esta página podrás crear un curso';
+});
+
+//ruta capturar parametros get
+Route::get('cursos/{curso}', function ($curso) {
+    return 'Bienvenido al curso: ' . $curso;
+});
+
+Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
+    if ($categoria) {
+        return "Bienvenido al curso: $curso, de la categoria: $categoria";
+    } else {
+        return "Bienvenido al curso: $curso";
+    }
+});
